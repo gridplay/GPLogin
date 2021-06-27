8aca70d2277b090b62b3d34e148e3c83515f76af
Chrisx84 <chrisx84@live.ca>
2021-06-27 11:11:21 -0400

    inital push

---
+++ Provider.php
@@ -0,0 +1,46 @@
+<?php
+namespace GPLogin;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+class Provider extends AbstractProvider {
+    public const IDENTIFIER = 'GPLogin';
+    protected $scopes = ['identify','uuid'];
+    protected $scopeSeparator = ' ';
+    protected function getAuthUrl($state) {
+        return $this->buildAuthUrlFromBase(
+            'https://gridplay.net/oauth2/authorize',
+            $state
+        );
+    }
+    protected function getTokenUrl() {
+        return 'https://gridplay.net/oauth2/token';
+    }
+    protected function getUserByToken($token) {
+        $response = $this->getHttpClient()->get(
+            'https://gridplay.net/api/users/@me',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+    protected function mapUserToObject(array $user) {
+        return (new User())->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['name'],
+            'name'     => $user['name'],
+            'uuid'     => $user['uuid'],
+        ]);
+    }
+    protected function getTokenFields($code) {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+    public static function additionalConfigKeys() {
+        return [];
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # GPLogin
- 
+
+```bash
+composer require gridplay/gplogin
+```
+## Installation & Basic Usage
+
+Please see the [Base Installation Guide](https://socialiteproviders.com/usage/), then follow the provider specific instructions below.
+
+### Add configuration to `config/services.php`
+
+```php
+'gplogin' => [
+  'client_id' => env('GPLOGIN_CLIENT_ID'),
+  'client_secret' => env('GPLOGIN_CLIENT_SECRET'),
+  'redirect' => env('GPLOGIN_REDIRECT_URI'),
+],
+```
+### Add provider event listener
+
+Configure the package's listener to listen for `SocialiteWasCalled` events.
+
+Add the event to your `listen[]` array in `app/Providers/EventServiceProvider`. See the [Base Installation Guide](https://socialiteproviders.com/usage/) for detailed instructions.
+
+```php
+protected $listen = [
+    \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+        // ... other providers
+        'GPLogin\\GPLoginExtendSocialite@handle',
+    ],
+];
+```
+
+### Usage
+
+You should now be able to use the provider like you would regularly use Socialite (assuming you have the facade installed):
+
+```php
+return Socialite::driver('gplogin')->redirect();
+```
+
+### Returned User fields
+
+- ``id``
+- ``name``
+- ``uuid``
